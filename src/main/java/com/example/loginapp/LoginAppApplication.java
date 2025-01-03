92b44c68f48d9b7c34849d96a94dbe6adf78d976
Harsh Gaikwad <98576491+HarshG777@users.noreply.github.com>
2025-01-03 13:15:16 +0530

    Update LoginAppApplication.java

--- src/main/java/com/example/loginapp/LoginAppApplication.java
+++ src/main/java/com/example/loginapp/LoginAppApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LoginAppApplication {
     public static void main(String[] args) {
         SpringApplication.run(LoginAppApplication.class, args);
+        SpringApplication.run(LoginAppApplication.class, args);
     }
 }
