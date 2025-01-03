e99943de07b0f457c102755d6cde7dd645742208
Harsh Gaikwad <98576491+HarshG777@users.noreply.github.com>
2025-01-03 11:46:12 +0530

    Create LoginControllerTest.java

---
+++ src/test/java/LoginControllerTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class LoginControllerTest {
+
+    @Autowired
+    private LoginController loginController;
+
+    @Test
+    public void contextLoads() {
+        assertNotNull(loginController);
+    }
+
+    // Add more tests for the methods in LoginController
+}
